5fb119d667fd0d28989b5bf8719391396fa91735
“Prof <prof@liontechacademy.com>
2024-02-13 20:37:07 -0500

    my first maven project

---
+++ sportsapp/src/test/java/com/liontech/sportsapp/SportsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liontech.sportsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
